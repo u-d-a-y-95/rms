00dbd67a1c86e0b46de4b3cf0b2811e39eb909d1
UDay <uday.dev.bd@gmail.com>
2022-12-11 10:45:00 +0600

    setup react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,22 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "antd/dist/reset.css";
 import AppLayout from "../components/layout";
+import { useState } from "react";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
